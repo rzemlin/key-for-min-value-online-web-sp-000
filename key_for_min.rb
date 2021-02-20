3861070afffcb1ef6b43a4109a1a9ddc44e65667
Ryan Zemlin <rzemlin@gmail.com>
2021-02-20 13:54:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+cities = {NY: 50000000, LA: 40000000, SF: 30000000}
+current_min_value = nil
+current_key_value = nil
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |key, value|
+    if 
+      current_min_value = nil
+      current_min_value = value
+      current_key_value = key
+    else
+      current_min_value > current_min_value
+      current_min_value = value
+      current_key_value = key
+      
+    end
+  end
+current_key_value
 end
