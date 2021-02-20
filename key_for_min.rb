36e610563b1b2b06aa3813b9f39dc7a823d34543
Ryan Zemlin <rzemlin@gmail.com>
2021-02-20 14:03:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,15 +1,15 @@
 cities = {NY: 50000000, LA: 40000000, SF: 30000000}
-current_min_value = nil
-current_key_value = nil
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
+ current_min_value = nil
+ current_key_value = nil
+   name_hash.each do |key, value|
     if 
-      current_min_value = nil
-      current_min_value = "#{value}"
-      current_key_value = "#{key}"
+      current_min_value == nil || current_min_value < value
+      current_min_value = value
+      current_key_value = {key}
     else
-      current_min_value < "#{value}"
+      
       current_min_value = "#{value}"
       current_key_value = "#{key}"
       
